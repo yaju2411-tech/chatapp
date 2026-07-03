19dbb048ad06c6f852957432511b51c1693e6e44
Yajuvendrasinh J Parmar <yaju2411@gmail.com>
2026-07-03 17:56:54 +0530

    change nodemailer for testing

--- backend/src/config/nodemailer.js
+++ backend/src/config/nodemailer.js
@@ -2,10 +2,22 @@ import dotenv from "dotenv";
 import nodemailer from "nodemailer";
 
 dotenv.config();
+
 export const transporter = nodemailer.createTransport({
-    service:"gmail",
-    auth:{
-        user:process.env.EMAIL_USER,
-        pass:process.env.EMAIL_PASSWORD
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    family: 4,          // Force IPv4
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+    },
+});
+
+transporter.verify((err, success) => {
+    if (err) {
+        console.error("SMTP Error:", err);
+    } else {
+        console.log("SMTP Ready");
     }
 });
